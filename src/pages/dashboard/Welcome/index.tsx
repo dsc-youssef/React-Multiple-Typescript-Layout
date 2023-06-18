10985c248c2de2424b9435b3c24d79cffa41a60f
youssef <youssef_bebawy@hotmail.com>
2023-06-18 22:41:14 +0300

    update some settings

--- src/pages/dashboard/Welcome/index.tsx
+++ src/pages/dashboard/Welcome/index.tsx
@@ -14,9 +14,7 @@ const DashboardWelcome: FC = () => {
         <div className="flex items-center justify-center flex-col">
           <img width={130} src={logo} alt="" />
           <h1 className="text-white text-3xl mb-5">Dashboard Layout</h1>
-
-          <NavLink to="/" className="bg-gradient-to-tr from-sky-800 to-blue-900 text-white py-2 px-5 rounded mt-3 shadow uppercase text-sm font-semibold transition hover:-translate-y-2 hover:shadow-md">website layout</NavLink>
-
+          <NavLink to="/" className="bg-gradient-to-tr from-sky-800 to-blue-900 text-white btn">website layout</NavLink>
           <div className="flex items-center justify-center gap-2 mt-5">
             <a className=" text-blue-500" href="https://www.facebook.com/YoussefBibawy1/" target="_blank">Facebook</a>
             <a className=" text-blue-500" href="https://github.com/" target="_blank">Github</a>
