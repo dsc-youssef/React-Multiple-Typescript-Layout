a349197f9b66c522b7443c0e42a5c1a75806bddc
youssef <youssef_bebawy@hotmail.com>
2023-06-18 13:27:50 +0300

    add fonts family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 
 // Custom Config
 const colors = require('./config/tailwind/colors');
+const fontFamily = require('./config/tailwind/fonts');
 
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors
+      colors,
+      fontFamily
     },
+
   },
   plugins: [],
 }
