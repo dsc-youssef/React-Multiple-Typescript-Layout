d44e152694105cd6e650616b493a00588059ec2c
dsc-youssef <youssef_bebawy@hotmail.com>
2023-07-08 23:11:58 +0300

    Fix more things in layout

--- src/layouts/dashboard/Layout.tsx
+++ src/layouts/dashboard/Layout.tsx
@@ -11,21 +11,10 @@ import { Row, Col } from "react-bootstrap";
 // Config
 import LAYOUT from "@/config/layout";
 
-// Default Components
-const Sidebar = lazy(() => import("@/components/dashboard/Sidebar"));
-
 const DashboardLayout: FC = () => {
   return (
     <main className={LAYOUT.dashboard_prefix}>
-      <Row className="m-0 p-0">
-        <Col className="flex">
-          <Sidebar />
-          <div className="w-full">
-            <header>Header</header>
-            <Outlet />
-          </div>
-        </Col>
-      </Row >
+      <Outlet />
     </main >
   )
 }
